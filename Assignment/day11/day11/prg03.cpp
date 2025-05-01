0d898dae8a61bbd784bd32e97c238158d1651df0
suve0803 <suve0803@gmail.com>
2025-05-01 09:35:47 +0530

    updated

--- Assignment/day11/day11/prg03.cpp
+++ Assignment/day11/day11/prg03.cpp
@@ -1,9 +1,38 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+void leaders(int arr[], int n) {
+    int* leaders = new int[n];
+    int count = 0;  
 
+    int right = arr[n - 1];
+    leaders [count++] = right;  
 
+    
+    for (int i = n - 2; i >= 0; i--) {
+        if (arr[i] > right) {  
+            leaders[count++] = arr[i];  
+            right = arr[i];  
+        }
+    }
+
+    
+    for (int i = count - 1; i >= 0; i--) {
+        cout << leaders[i] << " ";
+    }
+    cout << endl;
+}
 
 int main() {
+    int n;
+    cin >> n;  
 
+     int* arr = new int[n];  
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    leaders(arr, n); 
+    return 0;
 }
