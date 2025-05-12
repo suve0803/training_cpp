df83a2bf1c6d1af79c769a3f9a089445c12a4758
suve0803 <suve0803@gmail.com>
2025-05-12 09:10:52 +0530

    updated

--- assessments/cppbasics/cppbasics/prg37.cpp
+++ assessments/cppbasics/cppbasics/prg37.cpp
@@ -1,8 +1,24 @@
 
 /* Write a Program to Check if the Given String is Palindrome or not Using Recursion */
+#include <iostream>
+#include <cstring>
 
-#include<iostream>
 using namespace std;
-int main() {
 
+int pal(char a[], int s, int e) {
+    if (s >= e) return 1;
+    if (a[s] != a[e]) return 0;
+    return pal(a, s + 1, e - 1);
+}
+
+int main() {
+    char str[100];
+    cin.getline(str, 100);
+
+    if (pal(str, 0, strlen(str) - 1))
+        cout << "Palindrome" << endl;
+    else
+        cout << "Not palindrome" << endl;
+
+    return 0;
 }
