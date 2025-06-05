484574db00165ecbc93d0c136f6b1efedbb4d131
suve0803 <suve0803@gmail.com>
2025-06-05 09:38:31 +0530

    updated

--- Assignment/day38/day38/prog02.cpp
+++ Assignment/day38/day38/prog02.cpp
@@ -1,60 +1,33 @@
 /*std::vector<string> – Batch Word Processing*/
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <string>
 
-char toLowerChar(char ch) {
-    if (ch >= 'A' && ch <= 'Z') {
-        return ch + ('a' - 'A');
-    }
-    return ch;
-}
-std::string toLowerString(const std::string& str) {
-    std::string lowerStr;
-    for (char ch : str) {
-        lowerStr += toLowerChar(ch);
-    }
-    return lowerStr;
-}
-void sortStrings(std::vector<std::string>& vec) {
-    for (size_t i = 0; i < vec.size(); i++) {
-        for (size_t j = i + 1; j < vec.size(); j++) {
-            if (vec[i] > vec[j]) {
-                std::string temp = vec[i];
-                vec[i] = vec[j];
-                vec[j] = temp;
-            }
-        }
-    }
-}
-void removeDuplicates(std::vector<std::string>& vec) {
-    std::vector<std::string> uniqueVec;
-    for (size_t i = 0; i < vec.size(); i++) {
-        if (i == 0 || vec[i] != vec[i - 1]) {
-            uniqueVec.push_back(vec[i]);
-        }
-    }
-    vec = uniqueVec;
-}
+using namespace std;
 
-int main() {
-    std::vector<std::string> words = { "Apple", "banana", "apple", "Orange", "BANANA", "grape" };
-    for (std::string& word : words) {
-        word = toLowerString(word);
-    }
+int main()
+{
+	vector <string> fruits = { "Apple", "banana", "apple", "Orange", "BANANA", "grape" };
 
-    sortStrings(words);
-    removeDuplicates(words);
+	for (auto& fruit : fruits)
+		cout << fruit << endl;
+	
 
-    std::cout << "[";
-    for (size_t i = 0; i < words.size(); i++) {
-        std::cout << "\"" << words[i] << "\"";
-        if (i < words.size() - 1) {
-            std::cout << ", ";
-        }
-    }
-    std::cout << "]" << std::endl;
+	for (auto& fruit : fruits)
+		transform(fruit.begin(), fruit.end(), fruit.begin(), ::tolower);
 
-    return 0;
+	sort(fruits.begin(), fruits.end(), [](string s1, string s2) { 
+		return  s1 < s2;
+		});
+
+	auto newEnd = unique(fruits.begin(), fruits.end());
+
+	fruits.erase(newEnd, fruits.end());
+
+	for (auto& fruit : fruits)
+		cout << fruit << endl;
+	
+	return 0;
+
 }
-
---
+++ Assignment/day38/day38/prog06.cpp
@@ -0,0 +1,9 @@
+/*Banned Words Filter*/
+
+
+#include<iostream>
+#include<set>
+using namespace std;
+int main() {
+
+}
